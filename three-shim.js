d80f508686337c457ec28d3e8f90b0db658e5091
RhettArk <rhett@arkstrategic.io>
2025-09-10 23:53:37 -0400

    Fix import map JSON and remap unpkg three to shim

--- three-shim.js
+++ three-shim.js
@@ -1,6 +1,5 @@
 // three-shim.js
 export * from "https://unpkg.com/three@0.176.0/build/three.module.js?module&orig=1";
-
 export function hasOwnProperty(prop) {
   return Object.prototype.hasOwnProperty.call(this, prop);
 }
