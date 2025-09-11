1edd3fd1f04fd2ea618b74bf3f8020af49704c66
RhettArk <rhett@arkstrategic.io>
2025-09-10 23:45:05 -0400

    Import-map: only remap three.module.js to shim; leave three.core.js untouched

--- three-shim.js
+++ three-shim.js
@@ -1,8 +1,6 @@
 // three-shim.js
-export * from 'https://unpkg.com/three@0.176.0/build/three.module.js';
+export * from "https://unpkg.com/three@0.176.0/build/three.module.js?module&orig=1";
 
-// Provide a method so the namespace has a callable `hasOwnProperty`
 export function hasOwnProperty(prop) {
-  // when called as t.hasOwnProperty(prop), `this` == namespace object
   return Object.prototype.hasOwnProperty.call(this, prop);
 }
